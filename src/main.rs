f36e14dd3d71c0732d4ff37246622d1788b600ba
Alexius Huang <alexius.huang@gmail.com>
2023-11-03 00:03:44 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rustful_json_server::{server, thread_pool};
+use rustful_json_server::server::Server;
 
 const DEFAULT_PORT: usize = 5000;
 const POOL_CAPACITY: usize = 4;
@@ -6,15 +6,8 @@ const POOL_CAPACITY: usize = 4;
 fn main() {
     // Clear up terminal and then position cursor at row 1 col 1
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-    
-    let listener = server::create(DEFAULT_PORT);
-    let pool = thread_pool::ThreadPool::new(POOL_CAPACITY);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        pool.execute(|| {
-            server::handle_connection(stream);
-        });
-    }
+    Server::new(DEFAULT_PORT)
+        .set_pool_capacity(POOL_CAPACITY)
+        .start();
 }
--- src/server.rs
+++ src/server.rs
@@ -10,40 +10,72 @@ use std::{
 use self::response::ResponseBuilder;
 use self::request::Request;
 use self::status_code::StatusCode;
+use crate::thread_pool::ThreadPool;
 
-pub fn create(port: usize) -> TcpListener {
-    let host = format!("localhost:{port}");
-    println!("Listening on localhost:{port}...");
-
-    let listener = TcpListener::bind(host).unwrap();
-    listener
+pub struct Server {
+    listener: TcpListener,
+    pool_capacity: Option<usize>
 }
 
-pub fn handle_connection(mut stream: TcpStream) {
-    let request = Request::new(&mut stream);
+const DEFAULT_POOL_CAPACITY: usize = 4;
 
-    request.log();
+impl Server {
+    pub fn new(port: usize) -> Self {
+        let host = format!("localhost:{port}");
+        println!("Listening on localhost:{port}...");
+    
+        let listener = TcpListener::bind(host).unwrap();
+        Self { listener, pool_capacity: None }
+    }
 
-    // let (status_desc, file_path) = match request_line.as_str() {
-    //     "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        // "GET /sleep HTTP/1.1" => {
-        //     /* Simulating Slow Response... */
-        //     thread::sleep(Duration::from_secs(5));
-        //     ("HTTP/1.1 200 OK", "hello.html")
-        // },
-    //     _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
-    // };
+    pub fn set_pool_capacity(mut self, pool_capacity: usize) -> Self {
+        self.pool_capacity = Some(pool_capacity);
+        self
+    }
 
-    // let (content_length, contents) = read_content(file_path);
+    pub fn start(&self) {
+        let pool_capacity = if self.pool_capacity.is_some() {
+            self.pool_capacity.unwrap()
+        } else {
+            DEFAULT_POOL_CAPACITY
+        };
 
-    // let status_desc = "HTTP/1.1 200 OK";
-    // let content_length = format!("Content-Length: {}", request_line.len());
-    let response = ResponseBuilder::new()
-        .set_status_code(StatusCode::Ok)
-        .set_protocol(request.version)
-        .set_content(r#"{ "result": "Hello World!" }"#.to_owned())
-        .set_content_type("application/json".to_owned())
-        .build();
+        let pool = ThreadPool::new(pool_capacity);
 
-    stream.write_all(response.format().as_bytes()).unwrap();    
+        for stream in self.listener.incoming() {
+            let stream = stream.unwrap();
+    
+            pool.execute(|| {
+                Self::handle_connection(stream);
+            });
+        }    
+    }
+
+    pub fn handle_connection(mut stream: TcpStream) {
+        let request = Request::new(&mut stream);
+        request.log();
+    
+        // let (status_desc, file_path) = match request_line.as_str() {
+        //     "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+            // "GET /sleep HTTP/1.1" => {
+            //     /* Simulating Slow Response... */
+            //     thread::sleep(Duration::from_secs(5));
+            //     ("HTTP/1.1 200 OK", "hello.html")
+            // },
+        //     _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
+        // };
+    
+        // let (content_length, contents) = read_content(file_path);
+    
+        // let status_desc = "HTTP/1.1 200 OK";
+        // let content_length = format!("Content-Length: {}", request_line.len());
+        let response = ResponseBuilder::new()
+            .set_status_code(StatusCode::Ok)
+            .set_protocol(request.version)
+            .set_content(r#"{ "result": "Hello World!" }"#.to_owned())
+            .set_content_type("application/json".to_owned())
+            .build();
+    
+        stream.write_all(response.format().as_bytes()).unwrap();    
+    }    
 }
